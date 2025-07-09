ebe5b94a0591b953181a327fe643e2bcc4cd1d1a
Jona Fer <jonathan21javier@gmail.com>
2025-07-09 07:14:02 +0200

    Rimosso salve_mondo.c

--- salve_mondo.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>             // direttiva per il processore
-
-int main(){
-    printf("Salve Mondo dal mio programma C migliorato!\n");
-    printf("E' il mio primo progetto su GitHub. Che figata!\n");
-    return 0;
-}
